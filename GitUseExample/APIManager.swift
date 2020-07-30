680c7eb3d225ebef5fa8f8237e9d673e1df51cdb
Dhruv <dhruvvarde1@gmail.com>
2020-07-30 18:34:15 +0530

    -Added APIManager class

---
+++ GitUseExample/APIManager.swift
@@ -0,0 +1,13 @@
+//
+//  APIManager.swift
+//  GitUseExample
+//
+//  Created by theonetech on 30/07/20.
+//  Copyright © 2020 theonetech. All rights reserved.
+//
+
+import Foundation
+class APIManager: NSObject
+{
+    
+}
